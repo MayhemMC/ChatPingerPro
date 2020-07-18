d86ab36157483fedac001d7371b30bc2ef226281
Josh Merlino <joshua.merlino@gmail.com>
2020-07-18 19:54:17 -0400

    Add config registration and default config file

--- src/org/mayhemmc/chatpingerpro/Main.java
+++ src/org/mayhemmc/chatpingerpro/Main.java
@@ -1,9 +1,34 @@
 package org.mayhemmc.chatpingerpro;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 
+	// Create variable to store Main instance in later.
+	public static Main instance;
+	public static FileConfiguration cfg;
 	
+	@Override
+	public void onEnable() {
+		
+		// Register instance of Main.
+		instance = this;
+
+		// Save default config file.
+		this.saveDefaultConfig();
+		
+		// Save current config into `cfg`.
+		cfg = this.getConfig();
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+		// Unregister Main instance.
+		instance = null;
+		
+	}
 	
 }
