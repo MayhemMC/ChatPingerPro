ef9c2e023467c629d5dc50fa9d9467f236e252c1
Josh Merlino <joshua.merlino@gmail.com>
2020-07-20 12:05:42 -0400

    Update project setup

--- src/org/mayhemmc/chatpingerpro/Main.java
+++ src/org/mayhemmc/chatpingerpro/Main.java
@@ -1,10 +1,16 @@
 package org.mayhemmc.chatpingerpro;
 
+import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
 
 public class Main extends JavaPlugin implements Listener {
 
@@ -15,7 +21,7 @@ public class Main extends JavaPlugin implements Listener {
 	@Override
 	public void onEnable() {
 
-		// Register instance of Main.
+		// Register instance of
 		instance = this;
 
 		// Save default config file.
@@ -40,14 +46,60 @@ public class Main extends JavaPlugin implements Listener {
 	@EventHandler
 	public void onChat(AsyncPlayerChatEvent event) {
 
-		// If pings are enabled in the config...
-		if(cfg.getBoolean("pings.enable")) new ChatPing(event);
+		// Get the instance of the player that sent the message
+		Player sender = event.getPlayer();
 
-		// If items are enabled in the config...
-		// if(cfg.getBoolean("items.enable")) new ChatItem(event);
+		// Get chat message as array of words
+		String[] words = event.getMessage().split(" ");
 
-		// If inventory are enabled in the config...
-		// if(cfg.getBoolean("inventory.enable")) new ChatInventory(event);
+		// Build message string
+		TextComponent component = new TextComponent(String.format(event.getFormat(), sender.getDisplayName(), ""));
+
+		// Iterate over words:
+		for(String word : words) {
+
+			boolean wasWordProcessed = false;
+
+			// If pings are enabled & Player has permission to use them
+			if(cfg.getBoolean("ping.enable") && (!cfg.getBoolean("ping.permission.use") || sender.hasPermission(cfg.getString("ping.permission.node")))) {
+
+				// Iterate over online players:
+				for (Player player : Bukkit.getServer().getOnlinePlayers()) {
+
+					// If word is a players name:
+					if(word.equalsIgnoreCase(player.getName())) {
+
+						wasWordProcessed = true;
+
+						// Formulate hover text
+						ComponentBuilder hover = new ComponentBuilder();
+						for(String line : cfg.getStringList("ping.tooltip")) hover.appendLegacy(new PlaceholderParser(line + "&r\n").parseAs(player));
+						hover.removeComponent(hover.getParts().size() -1);
+
+						// Parse the placeholders and add it to the new component
+						TextComponent ping = new TextComponent(new PlaceholderParser(cfg.getString("ping.format")).parseAs(event.getPlayer()));
+						ping.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, hover.create()));
+					  	component.addExtra(ping);
+
+					}
+
+				}
+
+			}
+
+			// If the word wasnt an online players name, just add the word into the component
+			if(!wasWordProcessed) component.addExtra(new TextComponent(word));
+
+			// Ensure that spaces are put back inline
+			component.addExtra(" ");
+
+		}
+
+		// Cancel origonal chat event
+		event.setCancelled(true);
+
+		// Send JSON message to all players
+		for (Player player : event.getRecipients()) player.spigot().sendMessage(component);
 
 	}
 
